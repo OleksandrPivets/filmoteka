6ba9b8d05516a892550dea946b3001b1d71b42d7
Dartena <82412028+Dartena@users.noreply.github.com>
2021-11-18 21:49:32 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './sass/main.scss';
+import ApiService from './js/apiService';
+const apiService = new ApiService();
+async function getTrendingMovies() {
+  apiService.setPage(5);
+    const result = await apiService.getTrendingMovies();
+    const genres = await apiService.getGenres();
+    console.log(result);
+    console.log(genres);
+}
+getTrendingMovies();
