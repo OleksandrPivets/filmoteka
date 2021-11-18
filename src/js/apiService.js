8cc5023d224285cf53d60dcbd22cdec1beaffdfc
Oleksandra-Mas <sashamasyuk0702@gmail.com>
2021-11-18 21:53:41 +0200

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -8,17 +8,27 @@ export default class ApiService {
   async getTrendingMovies() {
     const url = `${BASE_URL}/trending/movie/day?page=${this.page}&api_key=${API_KEY}`;
     const response = await fetch(url);
-    return await response.json();
+    const result = await response.json();
+    result.results.map(movie => {
+      movie.backdrop_path = 'https://image.tmdb.org/t/p/w500' + movie.backdrop_path;
+    });
+    return result;
   }
   async searchMovie() {
     const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${this.searchQuery}&page=${this.page}&include_adult=false`;
     const response = await fetch(url);
-    return await response.json();
+    const result = await response.json();
+    result.results.map(movie => {
+      movie.backdrop_path = 'https://image.tmdb.org/t/p/w500' + movie.backdrop_path;
+    });
+    return result;
   }
   async getMovieById(id) {
     const url = `${BASE_URL}/movie/${id}?api_key=${API_KEY}`;
     const response = await fetch(url);
-    return await response.json();
+    const result = await response.json();
+    result.backdrop_path = 'https://image.tmdb.org/t/p/w500' + result.backdrop_path;
+    return result;
   }
   async getGenres() {
     const url = `${BASE_URL}/genre/movie/list?api_key=${API_KEY}`;
