cb65bf432843a250a137b216e2a1895c5c643e5d
Igor <polygor@gmail.com>
2021-11-24 11:51:07 +0200

    debug

--- src/js/modal.js
+++ src/js/modal.js
@@ -64,9 +64,10 @@ async function renderMovieInfo(id) {
   modalRefs.movieImg.src = `${movieInfo.poster_path}`;
   modalRefs.movieInfo.insertAdjacentHTML('beforeend', movieInfoTmp(movieInfo));
   // Добавляем индикатор задержки загрузки
-  const onLoadObj = document.querySelectorAll('.lightbox__content');
-  delayIndicator(onLoadObj,'classToInsertCodeAfter', 'movie-img', true);
-  
+  if (movieInfo.poster_path.substr(-4) != 'null') {
+    const onLoadObj = document.querySelectorAll('.lightbox__content');
+    delayIndicator(onLoadObj, 'classToInsertCodeAfter', 'movie-img', true);
+  };
   //Adding EventListeners
   modalRefs.button_watched = document.querySelector('.button-watched');
   modalRefs.button_queue = document.querySelector('.button-queue');
