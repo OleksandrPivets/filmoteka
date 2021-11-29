f745d2ecf31e00b6598187c910af8d77eb823e1e
Ekaterina Folkina <folkina7@gmail.com>
2021-11-29 14:47:55 +0200

    прошло автоматическое исправление разметки

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,6 +1,12 @@
-import markupMovie from "../templates/card.hbs";
-import { apiService, refs } from "../js/variables.global";
-import { renderOnStart, totalPages, renderTrending, renderSearchResults, searchQuerySaved } from './main-page-rendering';
+import markupMovie from '../templates/card.hbs';
+import { apiService, refs } from '../js/variables.global';
+import {
+  renderOnStart,
+  totalPages,
+  renderTrending,
+  renderSearchResults,
+  searchQuerySaved,
+} from './main-page-rendering';
 
 // export default refs = {
 //   paginationList: document.querySelector('.pagination-box'),
@@ -21,7 +27,7 @@ async function renderPagesList(apiPage) {
   refs.pageList.innerHTML = '';
   const start = currentPage - pageRange;
   const end = currentPage + pageRange;
-  
+
   for (let i = start; i <= end; i += 1) {
     if (i > 0 && i <= totalPages) {
       refs.pageList.insertAdjacentHTML(
@@ -32,18 +38,17 @@ async function renderPagesList(apiPage) {
   }
 }
 
-
 refs.paginationList.addEventListener('click', onBtnClick);
 refs.prevBtn.addEventListener('click', onPrevBtnClick);
 refs.nextBtn.addEventListener('click', onNextBtnClick);
 
 function onBtnClick(e) {
   e.preventDefault();
-  
+
   if (e.target.nodeName !== 'BUTTON') {
     return;
   }
- scrollPage()
+  scrollPage();
   refs.movieGallery.innerHTML = '';
   refs.pageList.innerHTML = '';
 
@@ -52,89 +57,90 @@ function onBtnClick(e) {
 
   if (apiService.query) {
     renderSearchResults(searchQuerySaved);
+  } else {
+    renderTrending();
   }
-  else {
-    
-  renderTrending()}
 }
 
 function onPrevBtnClick(e) {
   e.preventDefault();
- 
+
   if (currentPage > 1) {
     currentPage -= 1;
-    apiService.setPage(currentPage)
+    apiService.setPage(currentPage);
     refs.movieGallery.innerHTML = '';
     refs.pageList.innerHTML = '';
-    
-    
+
     scrollPage();
   }
   if (apiService.query) {
     renderSearchResults(searchQuerySaved);
+  } else {
+    renderTrending();
   }
-  else {
-    
-  renderTrending()}
 }
 
- function onNextBtnClick(e) {
-   e.preventDefault();
-   
+function onNextBtnClick(e) {
+  e.preventDefault();
+
   if (currentPage !== totalPages) {
     currentPage += 1;
-    
-   }
-    scrollPage();
-    refs.movieGallery.innerHTML = '';
-    refs.pageList.innerHTML = '';
-    apiService.setPage(currentPage) 
-   if (apiService.query) {
-    renderSearchResults(searchQuerySaved);
   }
-  else {
-    
-  renderTrending()}
-   
+  scrollPage();
+  refs.movieGallery.innerHTML = '';
+  refs.pageList.innerHTML = '';
+  apiService.setPage(currentPage);
+  if (apiService.query) {
+    renderSearchResults(searchQuerySaved);
+  } else {
+    renderTrending();
+  }
 }
 function isHideBtn() {
   if (currentPage === 1) {
-    refs.prevBtn.disabled = true
+    refs.prevBtn.disabled = true;
+  } else {
+    refs.prevBtn.disabled = false;
   }
-  else {refs.prevBtn.disabled = false}
-   if (currentPage === totalPages) {
-    refs.nextBtn.disabled = true
+  if (currentPage === totalPages) {
+    refs.nextBtn.disabled = true;
+  } else {
+    refs.nextBtn.disabled = false;
   }
-  else {refs.nextBtn.disabled = false}
- }
+}
 function activeBtn() {
-let pagesItem = refs.pageList.querySelectorAll('button');
+  let pagesItem = refs.pageList.querySelectorAll('button');
   for (let i = 0; i < pagesItem.length; i += 1) {
     if (Number(pagesItem[i].textContent) === currentPage) {
       pagesItem[i].classList.add('active-btn');
     }
-    if (Number(pagesItem[i].textContent) !== currentPage && pagesItem[i].classList.contains('active-btn')) {
-        pagesItem[i].classList.remove('active-btn');
-      }
+    if (
+      Number(pagesItem[i].textContent) !== currentPage &&
+      pagesItem[i].classList.contains('active-btn')
+    ) {
+      pagesItem[i].classList.remove('active-btn');
     }
   }
-
-
-function hideFirstLastBtn() {
-  if (currentPage < 4) { refs.firstPage.hidden = true }
-  else { refs.firstPage.hidden = false };
-  if (currentPage > totalPages - 3) { refs.lastPage.hidden = true }
-  else { refs.lastPage.hidden = false };
 }
 
+function hideFirstLastBtn() {
+  if (currentPage < 4) {
+    refs.firstPage.hidden = true;
+  } else {
+    refs.firstPage.hidden = false;
+  }
+  if (currentPage > totalPages - 3) {
+    refs.lastPage.hidden = true;
+  } else {
+    refs.lastPage.hidden = false;
+  }
+}
 
 function scrollPage() {
-    refs.header.scrollIntoView({
+  refs.header.scrollIntoView({
     behavior: 'smooth',
     block: 'end',
   });
 }
 
-export {
-  renderPagesList, hideFirstLastBtn, isHideBtn, activeBtn
-}
+export { renderPagesList, hideFirstLastBtn, isHideBtn, activeBtn };
